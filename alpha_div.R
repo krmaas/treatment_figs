7f79de9702e4e8372262c6038c98287ad29a0f24
krmaas <kendra.r.maas@gmail.com>
2014-07-08 16:12:19 -0700

    added standard error

--- alpha_div.R
+++ alpha_div.R
@@ -65,6 +65,10 @@ even <- ggplot(data=subset(b.a.rel.env, b.a.rel.env$om<98), aes(x=factor(om), y=
   ggtitle("Bacterial Relative to Reference")
 even
 
+sobs <- ggplot(data=subset(b.a.rel.env, b.a.rel.env$om<98), aes(x=factor(om), y=sobs))+
+  geom_boxplot()+
+  ggtitle("Bacterial Relative to Reference")
+sobs
 
 cov <- ggplot(data=subset(f.a.rel.env, f.a.rel.env$om<98), aes(x=factor(om), y=coverage))+
   geom_boxplot()+
@@ -81,8 +85,96 @@ even <- ggplot(data=subset(f.a.rel.env, f.a.rel.env$om<98), aes(x=factor(om), y=
   ggtitle("Fungal Relative to Reference")
 even
 
+sobs <- ggplot(data=subset(f.a.rel.env, f.a.rel.env$om<98), aes(x=factor(om), y=sobs))+
+  geom_boxplot()+
+  ggtitle("Fungal Relative to Reference")
+even
+
+#not seeing tons of trends with the box plots
+b.cov <- aov(coverage~factor(om), data=b.a.rel.env)
+summary(b.cov)
+TukeyHSD(b.cov)
+
+b.invs <- aov(invsimpson~factor(om), data=b.a.rel.env)
+summary(b.invs)
+TukeyHSD(b.invs)
+
+b.even <- aov(shannoneven~factor(om), data=b.a.rel.env)
+summary(b.even)
+TukeyHSD(b.even)
+
+b.sobs <- aov(sobs~factor(om), data=b.a.rel.env)
+summary(b.sobs)
+TukeyHSD(b.sobs)
 
 
+f.cov <- aov(coverage~factor(om), data=f.a.rel.env)
+summary(f.cov)
+TukeyHSD(f.cov)
+
+f.invs <- aov(invsimpson~factor(om), data=f.a.rel.env)
+summary(f.invs)
+TukeyHSD(f.invs)
+
+f.even <- aov(shannoneven~factor(om), data=f.a.rel.env)
+summary(f.even)
+TukeyHSD(f.even)
+
+f.sobs <- aov(sobs~factor(om), data=f.a.rel.env)
+summary(f.sobs)
+TukeyHSD(f.sobs)
 
 
+#alpha's vs zone and hor
+b.cov.hor <- aov(coverage~factor(horizon), data=b.a.rel.env)
+summary(b.cov.hor)
+TukeyHSD(b.cov.hor)
+
+b.invs.hor <- aov(invsimpson~factor(horizon), data=b.a.rel.env)
+summary(b.invs.hor)
+TukeyHSD(b.invs.hor)
+
+b.even.hor <- aov(shannoneven~factor(horizon), data=b.a.rel.env)
+summary(b.even.hor)
+TukeyHSD(b.even.hor)
+
+b.sobs.hor <- aov(sobs~factor(horizon), data=b.a.rel.env)
+summary(b.sobs.hor)
+TukeyHSD(b.sobs.hor)
+
+
+f.cov.hor <- aov(coverage~factor(horizon), data=f.a.rel.env)
+summary(f.cov.hor)
+TukeyHSD(f.cov.hor)
+
+f.invs.hor <- aov(invsimpson~factor(horizon), data=f.a.rel.env)
+summary(f.invs.hor)
+TukeyHSD(f.invs.hor)
+
+f.even.hor <- aov(shannoneven~factor(horizon), data=f.a.rel.env)
+summary(f.even.hor)
+TukeyHSD(f.even.hor)
+
+f.sobs.hor <- aov(sobs~factor(horizon), data=f.a.rel.env)
+summary(f.sobs.hor)
+TukeyHSD(f.sobs.hor)
+
+
+# #run plots one at a time
+# plot(b.cov)
+# plot(b.invs)
+# plot(b.even)
+# plot(b.sobs)
+
+
+######really basic stats on the data
+#create function that calculates standard error of the mean
+sem <- function(x)
+   {
+       sqrt(var(x)/length(x))
+    }
+
+
+sem(f.a$coverage)
+sem(b.a$coverage)
 
